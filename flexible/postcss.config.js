31df1054bf8a8bdcbe6ff1818190e4df97139a52
zhang <384515231@qq.com>
2024-07-14 11:46:48 +0800

    feat: 移动端适配方案

---
+++ flexible/postcss.config.js
@@ -0,0 +1,16 @@
+// https://cli.vuejs.org/zh/guide/css.html#postcss
+// vant适配了 viewport https://youzan.github.io/vant/#/zh-CN/quickstart#shi-li-gong-cheng
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      browsers: 'last 2 versions'
+    },
+    'postcss-px2rem': {
+      remUnit: 37.5
+    }
+  }
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> f275f9e00e9c99edefd32b5b57acac9c9f17ccc3
